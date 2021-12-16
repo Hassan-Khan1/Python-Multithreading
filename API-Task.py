cbacd8b1c8b5b390d40fe9b6af76e975f738dc03
Hassan-Khan1 <hassan.jamal@mobylogix.com>
2021-12-16 18:58:25 +0500

    FC

--- API-Task.py
+++ API-Task.py
@@ -17,31 +17,35 @@ pak_json = r.json()
 
 
 
-urllib.request.urlretrieve('https://picsum.photos/id/0/5616/3744','Hassan.jpg')
+# urllib.request.urlretrieve('https://picsum.photos/id/0/5616/3744','Hassan.jpg')
 
     # urllib.request.urlretrieve(pakg_str, "/home/hassan/Desktop/Python Threading/DDDDDD.jpg")
     # urllib.request.urlretrieve(pakg_str, filename)
 
 
+list1 = ['https://picsum.photos/id/1023/3955/2094','https://picsum.photos/id/1024/1920/1280']
+n = len(list1)
+for i in range(n):
 
+  urllib.request.urlretrieve(list1[i],f'Hassan{i}.jpg')
 
+# n = 0
+# try:
+#   # for n in  range(40):
+#   while True:
+#     pakg_str = json.dumps(pak_json[n]['download_url'])
 
-n = 0
-try:
-  # for n in  range(40):
-  while True:
-    pakg_str = json.dumps(pak_json[n]['download_url'])
+#     print(pakg_str.replace('"',''))
+#     n = n+1
 
-    print(pakg_str.replace('"',''))
-    n = n+1
-
-    urllib.request.urlretrieve(pakg_str,f'Hassan{n}.jpg')
+    
+    # urllib.request.urlretrieve(pakg_str,f'Hassan.jpg')
 
     # urllib.request.urlretrieve(pakg_str, "/home/hassan/Desktop/Python Threading/DDDDDD.jpg")
     # urllib.request.urlretrieve(pakg_str, filename)
 
-except:
+# except:
   
-  print("Index is Out of Range")
+#   print("Index is Out of Range")
 
 
