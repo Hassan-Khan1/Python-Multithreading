01f0fb8a57a741c464bbb10ac0d1cae2641ecc1e
Hassan-Khan1 <hassan.jamal@mobylogix.com>
2021-12-17 15:29:00 +0500

    Working

--- 1.4-API-Task.py
+++ 1.4-API-Task.py
@@ -14,38 +14,30 @@ class ImageDownload:
       print("Image : Start")
       self.image()
       print("Image ENd")
-
       
 
   def image(self):
       # print("THread Name After: ",currentThread().getName())
 
       list = [] 
-
       random_num  = random.randint(0,25)
-
       pakg_str = json.dumps(pak_json[random_num]['download_url'])
       ss = pakg_str.replace('"','')
       list.insert(0,ss)
-
       ss = [random.choice('0123456789') for _ in range(1,5)]
       ss2 = "".join(ss)  
       # print(ss2 , time.thread_time())
-
-      Image1  = urllib.request.urlretrieve(list[0],f'Image{ss2}.jpg')
-        
+      Image1  = urllib.request.urlretrieve(list[0],f'Imagee{ss2}.jpg')
       print(ss2, ' : ',list[0],' :  ',currentThread().getName())
-
+      
 
 class CL:
   def PO(self):
     for n in range(10):
       print("Thread Start::  " , n)
-
       # IM = ImageDownload()
       t = Thread(target=ImageDownload,name = f"THread Number : {n}")
       t.start()
-
       print("Thread End::  " , n)
 
    
